8a5956ef99a125600fb9a3e355cee80903e5a3dd
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-03 21:36:10 +0200

    fetch someText.txt aadded

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 const button = document.getElementById('button');
 
+// Longer way:
+
+/*button.addEventListener('click', getText);
+
+function getText(){
+    fetch('someText.txt')
+    .then(function(resp){
+        return resp.text()
+    })
+    .then(function(data){
+        console.log(data)
+    })
+} */
+
 button.addEventListener('click', () =>{
-    console.log(`You've clicked me`)
-})
+    fetch('someText.txt')
+    .then((resp) => resp.text())
+    .then((data) => {
+        const textArea =document.getElementById('textConatainer');
+        textArea.innerHTML = data;
+    })
+    .catch((error)=>console.log('Error'))
+});
+
+
